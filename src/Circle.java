49c414154b7eb188061ffa2e6342993d7e6f4436
grzegorzkuczera <kuczera.grzegorz@gmail.com>
2022-07-03 16:37:36 +0200

    :qw :wq

---
+++ src/Circle.java
@@ -0,0 +1,18 @@
+public class Circle implements Shape {
+
+    private double radius;
+    final double pi = Math.PI;
+
+
+    @Override
+    public double getArea() {
+
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rectangle test = new Rectangle(8,1);
+
+    }
+}
