af72b166aa3f61e3cf2249ad687564b13a467118
grzegorzkuczera <kuczera.grzegorz@gmail.com>
2022-07-03 23:05:50 +0200

    with comment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rectangle test = new Rectangle(8,1);
+        Rectangle test = new Rectangle(12,12);
+        test.getPerimeter();
+        test.getArea();
+        System.out.println(test.getPerimeter());
+        System.out.println(test.getArea());
+
 
     }
 }
