a10f2336ecacfaa2d699a598edbb82d3d9164f31
grzegorzkuczera <kuczera.grzegorz@gmail.com>
2022-07-04 11:30:33 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rectangle test = new Rectangle(12,12);
-        test.getPerimeter();
-        test.getArea();
-        System.out.println(test.getPerimeter());
-        System.out.println(test.getArea());
+        Rectangle test = new Rectangle(12, 12);
+        System.out.println("Recnangle perimeter: " + test.getPerimeter());
+        System.out.println("Rectangle area: " + test.getArea());
+
+        Square test2 = new Square(11);
+        System.out.println("Square perimeter: " + test2.getPerimeter());
+        System.out.println("Square area: " + test2.getArea());
+
+
+
+
+
 
 
     }
