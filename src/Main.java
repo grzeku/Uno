b49d836c8895486159c590b0fbbc1447e6f41a1c
grzegorzkuczera <kuczera.grzegorz@gmail.com>
2022-07-05 17:51:58 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rectangle test = new Rectangle(12, 12);
+        Shape test = new Rectangle(12, 12);
         System.out.println("Recnangle perimeter: " + test.getPerimeter());
         System.out.println("Rectangle area: " + test.getArea());
 
-        Square test2 = new Square(11);
+        Shape test2 = new Square(11);
         System.out.println("Square perimeter: " + test2.getPerimeter());
         System.out.println("Square area: " + test2.getArea());
 
-        Circle test3 = new Circle(22);
+        Shape test3 = new Circle(22);
         System.out.println("Circle perimeter: " + test3.getPerimeter());
         System.out.println("Circle area: " + test3.getArea());
-
-
-
-
-
-
 
 
     }
